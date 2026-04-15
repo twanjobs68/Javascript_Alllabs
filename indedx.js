9850f90d5d15d993c9605baa949b6d5c66f191fc
Antwan Grant <twanjobs68@gmail.com>
2026-04-14 23:04:29 -0400

    added title comment to index file

---
+++ indedx.js
@@ -0,0 +1,18 @@
+//LAB 2.0.15 LAB Variables and Data Types
+
+const name1 = "Maxwell Wright";
+const name2 = "Raja Villarreal";
+const name3 = "Helen Richards";
+
+const phone1 = "(0191)719 6495";
+const phone2 = "0866 398 2895";
+const phone3 = "0800 1111";
+
+const email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+const email2 = "posuere.vulputate@sed.com";
+const email3 = "libero@convallis.edu";   
+
+
+console.log(name1 + "/" + phone1 + "/" + email1);
+console.log(name3 + "/" + phone3 + "/" + email3);
+
