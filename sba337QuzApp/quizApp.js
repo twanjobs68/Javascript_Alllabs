9fc29bd177329fbdfd01e8db7264d6e4617b5a56
Antwan Grant <twanjobs68@gmail.com>
2026-04-19 16:52:15 -0400

    Updat4ed se4ctions with comments and renamed some variables for clarity.

--- sba337QuzApp/quizApp.js
+++ sba337QuzApp/quizApp.js
@@ -1,6 +1,8 @@
+//Welcome message and prompt for user's name
 var name = prompt("What is your name?");
 alert("Welcome to the quiz, " + name + "! Get Ready.");
 
+//This is an array of quiz questions and answers
 const quizQuestions = [
   { question: "What color is an orange?", answer: "orange" },
   { question: "What is a baby kangaroo called?", answer: "joey" },
@@ -9,25 +11,39 @@ const quizQuestions = [
   { question: "What is the largest nocturnal bird in the world?", answer: "owl" }
 ];
 
-var currentQuestionIndex = 0;
+//initalize variables for array and scoring
+var currentQustnLctn = 0;
 var score = 0;
 
-function askQuestion() {
-  var answer = prompt(quizQuestions[currentQuestionIndex].question);
-  if (answer && answer.toLowerCase() === quizQuestions[currentQuestionIndex].answer) {
+//function ask question and check answer
+function theQuestionIs() {
+  var answer = prompt(quizQuestions[currentQustnLctn].question);
+
+  //This loop checks if answer is correct and adds to score if it is, otherwise alerts 
+  // user that answer is incorrect
+  // toLowerCase is used to make the answer case insensitive therefore
+  // allowing the user to input the answer in upper or lower case and still be correct    
+  if (answer && answer.toLowerCase() === quizQuestions[currentQustnLctn].answer) {
     alert("Correct!");
+    //Add 1 to score if correct
     score++;
   } else {
+    //Alert user if answer is incorrect
     alert("Incorrect.");
   }
-  currentQuestionIndex++;
+  //Add 1 to current question location of the array and go on to the next question
+  currentQustnLctn++;
+}
+//The while loop will continue to ask each question in the array until the end of the array 
+// is reached. The loop will then exit and alert the user that they have completed the quiz
+// and display their score out of the number of questions asked of the quiz.
+
+
+while (currentQustnLctn < quizQuestions.length) {
+  theQuestionIs();
 }
 
-while (currentQuestionIndex < quizQuestions.length) {
-  askQuestion();
-}
+alert("The Quiz is completed, " + name + "! Your score is: " + score + "/" + quizQuestions.length);
 
-alert("Quiz completed, " + name + "! Your score is: " + score + "/" + quizQuestions.length);
-}
 
 
